52b2ade561284d53d66410f4b1b867f06ddde070
LeonardoSoutoMaior <leo.soutomaior@hotmail.com>
2024-08-07 12:27:43 -0300

    Criação da Entidade e Repository de Condominio

---
+++ src/main/java/com/example/gestao/domain/condominio/Condominio.java
@@ -0,0 +1,32 @@
+package com.example.gestao.domain.condominio;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Condominio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "endereco")
+    private String endereco;
+
+    @Column(name = "quantidade_de_unidades")
+    private Integer quantidadeDeUnidades;
+}
---
+++ src/main/java/com/example/gestao/repositories/CondominioRepository.java
@@ -0,0 +1,7 @@
+package com.example.gestao.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CondominioRepository extends JpaRepository {
+
+}
