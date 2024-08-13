0ad9949607b09d068f866b53d320f7b2503279cc
LeonardoSoutoMaior <leo.soutomaior@hotmail.com>
2024-08-13 11:50:45 -0300

    Possibilidade de buscar todas unidades

--- src/main/java/com/example/gestao/controller/UnidadeController.java
+++ src/main/java/com/example/gestao/controller/UnidadeController.java
@@ -1,13 +1,13 @@
 package com.example.gestao.controller;
 
 import com.example.gestao.domain.unidade.Unidade;
+import com.example.gestao.repositories.UnidadeRepository;
 import com.example.gestao.service.UnidadeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/unidades")
@@ -16,9 +16,18 @@ public class UnidadeController {
     @Autowired
     private UnidadeService unidadeService;
 
+    @Autowired
+    private UnidadeRepository unidadeRepository;
+
     @PostMapping
     public ResponseEntity<Unidade> criarUnidade(@RequestBody Unidade unidade){
         Unidade novaUnidade = unidadeService.criarUnidade(unidade);
         return ResponseEntity.ok(novaUnidade);
+    }
+
+    @GetMapping("/todasUnidades")
+    public ResponseEntity<List<Unidade>> retornaUnidades(){
+        List<Unidade> unidades = unidadeRepository.findAll();
+        return ResponseEntity.ok(unidades);
     }
 }
