dc887bd0ba94f50f4fd48bc4caa5551155afe63e
LeonardoSoutoMaior <leo.soutomaior@hotmail.com>
2024-08-15 16:55:44 -0300

    Possibilidade de atualizar, deletar, listar e buscar reuniao por id

--- src/main/java/com/example/gestao/controller/ReuniaoController.java
+++ src/main/java/com/example/gestao/controller/ReuniaoController.java
@@ -1,13 +1,14 @@
 package com.example.gestao.controller;
 
 import com.example.gestao.domain.reuniao.Reuniao;
+import com.example.gestao.domain.tarefa.Tarefa;
 import com.example.gestao.service.ReuniaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/reuniao")
@@ -20,5 +21,29 @@ public class ReuniaoController {
     public ResponseEntity<Reuniao> criarReuniao(@RequestBody Reuniao reuniao){
         Reuniao novaReuniao = reuniaoService.criarReuniao(reuniao);
         return ResponseEntity.ok(novaReuniao);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reuniao> buscarReuniaoPorId(@PathVariable UUID id){
+        Reuniao reuniao = reuniaoService.buscarReuniaoPorId(id);
+        return ResponseEntity.ok(reuniao);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Reuniao>> listarReunioes(){
+        List<Reuniao> reunioes = reuniaoService.listarTarefas();
+        return ResponseEntity.ok(reunioes);
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<Reuniao> atualizarReuniao(@PathVariable UUID id, @RequestBody Reuniao reuniao){
+        Reuniao reuniaoAtualizada = reuniaoService.atualizarReuniao(id, reuniao);
+        return ResponseEntity.ok(reuniao);
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Void> deletarReuniao(@PathVariable UUID id){
+        reuniaoService.deletarReuniao(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/example/gestao/service/ReuniaoService.java
+++ src/main/java/com/example/gestao/service/ReuniaoService.java
@@ -2,9 +2,13 @@ package com.example.gestao.service;
 
 import com.example.gestao.domain.condominio.Condominio;
 import com.example.gestao.domain.reuniao.Reuniao;
+import com.example.gestao.domain.tarefa.Tarefa;
 import com.example.gestao.repositories.ReuniaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ReuniaoService {
@@ -19,5 +23,30 @@ public class ReuniaoService {
         Condominio condominio = condominioService.buscarCondominioPorId(reuniao.getCondominio().getId());
         reuniao.setCondominio(condominio);
         return reuniaoRepository.save(reuniao);
+    }
+
+    public Reuniao buscarReuniaoPorId(UUID id){
+        return reuniaoRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Reunião não encontrada"));
+    }
+
+    public List<Reuniao> listarTarefas(){
+        return reuniaoRepository.findAll();
+    }
+
+    public Reuniao atualizarReuniao(UUID id, Reuniao reuniao){
+        Reuniao reuniaoExistente = buscarReuniaoPorId(id);
+
+        reuniaoExistente.setDataReuniao(reuniao.getDataReuniao());
+        reuniaoExistente.setHoraReuniao(reuniao.getHoraReuniao());
+        reuniaoExistente.setLocalReuniao(reuniao.getLocalReuniao());
+        reuniaoExistente.setPauta(reuniaoExistente.getPauta());
+
+        return reuniaoRepository.save(reuniaoExistente);
+    }
+
+    public void deletarReuniao(UUID id){
+        Reuniao reuniao = buscarReuniaoPorId(id);
+        reuniaoRepository.delete(reuniao);
     }
 }
